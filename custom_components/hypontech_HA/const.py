023996797ea9a7b8c07e3ed133d11557a0c5b455
jon7119 <168350835+jon7119@users.noreply.github.com>
2025-07-08 12:21:00 +0200

    Delete custom_components/hypontech_HA directory

--- custom_components/hypontech_HA/const.py
+++
@@ -1,9 +0,0 @@
-"""
-Constants for Hypontech HA integration.
-"""
-
-DOMAIN = "hypontech_ha"
-
-CONF_HYPONTECH_USERNAME = "hypontech_username"
-CONF_HYPONTECH_PASSWORD = "hypontech_password"
-CONF_SYSTEM_ID = "system_id"
